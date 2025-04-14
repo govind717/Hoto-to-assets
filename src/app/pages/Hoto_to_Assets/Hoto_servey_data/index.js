095cf270f5249a810247aad2516ed3d822de38c0
AbhiKdc <abhishek.vishwakarma@kdigitalcurry.com>
2025-04-14 15:12:39 +0530

    14/4/25 3:12 pm

--- src/app/pages/Hoto_to_Assets/Hoto_servey_data/index.js
+++ src/app/pages/Hoto_to_Assets/Hoto_servey_data/index.js
@@ -24,7 +24,7 @@ const Hoto_servey_data = () => {
                 <TabContext value={value}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                         <TabList onChange={handleChange} aria-label="lab API tabs example">
-                            <Tab label="GP-Wide Assets" value="1" />
+                            <Tab label="GP-Wise Assets" value="1" />
                             <Tab label="Asset Portfolio" value="2" />
                             <Tab label="Rectification" value="3" />
                             <Tab label="Transfer" value="4" />
